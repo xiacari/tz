7a11f81897aa54e2e9c4cd45a68e6dfe7603d7d8
IvanHome <mrobanash18022001@gmail.com>
2024-04-10 15:10:53 +0300

    Second Commit. Improved parser.
    added "list", "info" and "response" command to get parsed data's information.

--- Parser.cs
+++ Parser.cs
@@ -24,6 +24,15 @@
                 case "parse":
                     Parse(arguments);
                     break;
+                case "list":
+                    List(arguments);
+                    break;
+                case "response":
+                    Response(arguments);
+                    break;
+                case "info":
+                    Info(arguments);
+                    break;
                 case "exit":
                     return;
                 default:
@@ -40,8 +49,17 @@
         Console.WriteLine("    1: clear");
         Console.WriteLine("    Clears console.\n");
         Console.WriteLine("    2: parse [filePath]");
-        Console.WriteLine("    Parses file to blocks. Displays each block size.");
-        Console.WriteLine("    3: exit");
+        Console.WriteLine("    Parses file to blocks of responses.");
+        Console.WriteLine("    You need to use this command before using commands below.\n");
+        Console.WriteLine("    3: list");
+        Console.WriteLine("    Displays list of parsed blocks and responses. List indexes can be used as arguments.\n");
+        Console.WriteLine("    4: response [blockIndex] <responseIndex>");
+        Console.WriteLine("    Displays full information about specified response.");
+        Console.WriteLine("    If <responseIndex> left empty, selects the first response in block.\n");
+        Console.WriteLine("    5: info");
+        Console.WriteLine("    Displays every response type count and their average byte size.");
+        Console.WriteLine("    Also shows session's start and end approximate time.\n");
+        Console.WriteLine("    6: exit");
         Console.WriteLine("    Exits the program.\n");
     }
 
@@ -75,11 +93,98 @@
         }
         _blocks = blocks.ToArray();
         Console.WriteLine("    File parsed successfully.");
-        Console.WriteLine($"    Block count: {_blocks.Length}");
+        Console.WriteLine($"    Blocks: {_blocks.Length}");
+        Console.WriteLine();
+    }
+    private static void List(string[] args)
+    {
+        if (_blocks == null)
+        {
+            Console.WriteLine("    Nothing is parsed yet. Use \"parse\" command.");
+            return;
+        }
         for (var i = 0; i < _blocks.Length; i++)
         {
             Console.WriteLine($"    {i}: {_blocks[i]}");
+            for (var j = 0; j < _blocks[i].ResponseCount; j++)
+            {
+                Console.WriteLine($"        {j}: {_blocks[i].Responses[j]}");
+            }
         }
         Console.WriteLine();
+    }
+    private static void Response(string[] args)
+    {
+        if (args.Length < 1 || args.Length > 2)
+        {
+            Console.WriteLine("    Wrong arguments count.");
+            return;
+        }
+        if (!int.TryParse(args[0], out var blockIndex))
+        {
+            Console.WriteLine("    Block index must be a number");
+            return;
+        }
+        var responseIndex = 0;
+        if (args.Length == 2)
+        {
+            if (!int.TryParse(args[1], out responseIndex))
+            {
+                Console.WriteLine("    Response index must be a number");
+                return;
+            }
+        }
+        if (_blocks == null)
+        {
+            Console.WriteLine("    Nothing is parsed yet. Use \"parse\" command.");
+            return;
+        }
+        if (blockIndex < 0 || blockIndex >= _blocks.Length)
+        {
+            Console.WriteLine("    Block index is out of range.");
+            return;
+        }
+        var block = _blocks[blockIndex];
+        if (responseIndex < 0 || responseIndex >= block.ResponseCount)
+        {
+            Console.WriteLine("    Response index is out of range.");
+            return;
+        }
+
+        var response = _blocks[blockIndex].Responses[responseIndex];
+        Console.WriteLine($"    Type: {response.Type}");
+        Console.WriteLine($"    Compression format: {response.CompressionFormat}");
+        Console.WriteLine($"    Size: {response.Length}");
+        Console.WriteLine($"    Content: {response.Content}");
+        Console.WriteLine();
+    }
+
+    private static void Info(string[] args)
+    {
+        var responses = _blocks.SelectMany(x => x.Responses).ToArray();
+        var sorted = responses.GroupBy(x => x.Type)
+            .Select(x => x.ToArray())
+            .OrderByDescending(x => x.Length)
+            .ToArray();
+
+        for (var i = 0; i < sorted.Length; i++)
+        {
+            var avarageSize = (int)Math.Floor(sorted[i].Average(x => x.Length));
+            Console.WriteLine($"    {i}: {sorted[i][0].Type}");
+            Console.WriteLine($"      Count: {sorted[i].Length}");
+            Console.WriteLine($"      Average Size: {avarageSize}");
+        }
+
+        var firstDateTime = responses.FirstOrDefault(x => x.HasDateTime)?.DateTime;
+        var lastDateTime = responses.LastOrDefault(x => x.HasDateTime)?.DateTime;
+        if (firstDateTime != null && lastDateTime != null)
+        {
+            var sessionTime = lastDateTime - firstDateTime;
+            Console.WriteLine($"\n    Session time: {sessionTime?.ToString(@"hh\:mm\:ss")}\n");
+        }
+        else
+        {
+            Console.WriteLine("\n    Session time: Unknown\n");
+        }
     }
 }
