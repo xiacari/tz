fe39ff88be60fa7c1fb69cc7e6d7bab28550b42b
IvanHome <mrobanash18022001@gmail.com>
2024-04-13 09:13:11 +0300

    changed program controls

--- Parser.cs
+++ Parser.cs
@@ -1,4 +1,7 @@
-﻿namespace tz
+﻿using System.Text;
+using System.Xml.Linq;
+
+namespace tz
 {
     public static class Parser
     {
@@ -6,84 +9,49 @@
 
         public static void Main(string[] args)
         {
-            Console.WriteLine("Parser program is running. Type \"help\" to see full command list.");
-            while (true)
+            if (args.Length < 1)
             {
-                var input = Console.ReadLine();
-                if (input == null)
-                    continue;
-                var split = input.Split(' ');
-                var command = split[0];
-                var arguments = split[1..];
-                switch (command)
-                {
-                    case "help":
-                        Help(arguments);
-                        break;
-                    case "clear":
-                        Clear(arguments);
-                        break;
-                    case "parse":
-                        Parse(arguments);
-                        break;
-                    case "list":
-                        List(arguments);
-                        break;
-                    case "response":
-                        Response(arguments);
-                        break;
-                    case "info":
-                        Info(arguments);
-                        break;
-                    case "exit":
-                        return;
-                    default:
-                        Console.WriteLine("    Unknow command. Type \"help\" to see full command list.");
-                        break;
-                }
-            }
-        }
-
-        private static void Help(string[] args)
-        {
-            Console.WriteLine("    0: help");
-            Console.WriteLine("    Displays full list of commands.\n");
-            Console.WriteLine("    1: clear");
-            Console.WriteLine("    Clears console.\n");
-            Console.WriteLine("    2: parse [filePath]");
-            Console.WriteLine("    Parses file to blocks of responses.");
-            Console.WriteLine("    You need to use this command before using commands below.\n");
-            Console.WriteLine("    3: list");
-            Console.WriteLine("    Displays list of parsed blocks and responses. List indexes can be used as arguments.\n");
-            Console.WriteLine("    4: response [blockIndex] <responseIndex>");
-            Console.WriteLine("    Displays full information about specified response.");
-            Console.WriteLine("    If <responseIndex> left empty, selects the first response in block.\n");
-            Console.WriteLine("    5: info");
-            Console.WriteLine("    Displays every response type count and their average byte size.");
-            Console.WriteLine("    Also shows session's start and end approximate time.\n");
-            Console.WriteLine("    6: exit");
-            Console.WriteLine("    Exits the program.\n");
-        }
-
-        private static void Clear(string[] args)
-        {
-            Console.Clear();
-        }
-
-        private static void Parse(string[] args)
-        {
-            if (args.Length != 1)
-            {
-                Console.WriteLine("    Wrong arguments count.");
+                Console.WriteLine("Wrong arguments.");
                 return;
             }
-            var path = args[0];
+            var command = args[0];
+            var arguments = args[1..];
+
+            if (arguments.Length < 1)
+            {
+                Console.WriteLine("Wrong arguments.");
+                return;
+            }
+            var path = arguments[0];
             if (!File.Exists(path))
             {
                 Console.WriteLine("    File does not exits.");
                 return;
             }
+            ParseFile(path);
 
+            switch (command)
+            {
+                case "parse":
+                    Parse();
+                    break;
+                case "printall":
+                    PrintAll();
+                    break;
+                case "info":
+                    Info();
+                    break;
+                case "save":
+                    Save(path);
+                    break;
+                default:
+                    Console.WriteLine("Unknown command.");
+                    break;
+            }
+        }
+
+        private static void ParseFile(string path)
+        {
             var blocks = new List<Block>();
             var file = File.ReadAllBytes(path);
             var currentIndex = 0;
@@ -94,79 +62,75 @@
                 blocks.Add(block);
             }
             _blocks = blocks.ToArray();
-            Console.WriteLine("    File parsed successfully.");
-            Console.WriteLine($"    Blocks: {_blocks.Length}");
-            Console.WriteLine();
         }
-        private static void List(string[] args)
+
+        private static void Parse()
         {
-            if (_blocks == null)
-            {
-                Console.WriteLine("    Nothing is parsed yet. Use \"parse\" command.");
-                return;
-            }
+            Console.WriteLine(GetParse());
+            Console.WriteLine("File is parsed successfully.");
+        }
+
+        private static void PrintAll()
+        {
+            Console.WriteLine($"Blocks: {_blocks.Length}");
             for (var i = 0; i < _blocks.Length; i++)
             {
-                Console.WriteLine($"    {i}: {_blocks[i]}");
                 for (var j = 0; j < _blocks[i].ResponseCount; j++)
                 {
-                    Console.WriteLine($"        {j}: {_blocks[i].Responses[j]}");
+                    Console.WriteLine($"{i}.{j}:\n{GetResponse(i, j, Newtonsoft.Json.Formatting.None)}");
                 }
             }
-            Console.WriteLine();
         }
-        private static void Response(string[] args)
+
+        private static void Info()
         {
-            if (args.Length < 1 || args.Length > 2)
+            Console.WriteLine(GetInfo());
+        }
+
+        private static void Save(string path)
+        {
+            path += "_parsed";
+
+            if (!Directory.Exists(path))
+                Directory.CreateDirectory(path);
+            var fileInfo = GetParse() + "\n" + GetInfo();
+            File.WriteAllText(path + "/info.txt", fileInfo);
+
+            path += "/responses";
+            if (!Directory.Exists(path))
+                Directory.CreateDirectory(path);
+            for (var i = 0; i < _blocks.Length; i++)
             {
-                Console.WriteLine("    Wrong arguments count.");
-                return;
-            }
-            if (!int.TryParse(args[0], out var blockIndex))
-            {
-                Console.WriteLine("    Block index must be a number");
-                return;
-            }
-            var responseIndex = 0;
-            if (args.Length == 2)
-            {
-                if (!int.TryParse(args[1], out responseIndex))
+                for (var j = 0; j < _blocks[i].ResponseCount; j++)
                 {
-                    Console.WriteLine("    Response index must be a number");
-                    return;
+                    var responsePath = path + $"/{i}.{j}.txt";
+                    var response = GetResponse(i, j, Newtonsoft.Json.Formatting.Indented);
+                    File.WriteAllText(responsePath, response);
                 }
             }
-            if (_blocks == null)
-            {
-                Console.WriteLine("    Nothing is parsed yet. Use \"parse\" command.");
-                return;
-            }
-            if (blockIndex < 0 || blockIndex >= _blocks.Length)
-            {
-                Console.WriteLine("    Block index is out of range.");
-                return;
-            }
-            var block = _blocks[blockIndex];
-            if (responseIndex < 0 || responseIndex >= block.ResponseCount)
-            {
-                Console.WriteLine("    Response index is out of range.");
-                return;
-            }
-
-            var response = _blocks[blockIndex].Responses[responseIndex];
-            Console.WriteLine($"    Type: {response.Type}");
-            if (response.HasDateTime)
-            {
-                Console.WriteLine($"    Time: {response.DateTime}");
-            }
-            Console.WriteLine($"    Compression format: {response.CompressionFormat}");
-            Console.WriteLine($"    Size: {response.Length}");
-            Console.WriteLine($"    Content: {response.Content}");
-            Console.WriteLine();
+            Console.WriteLine("Parsed info is saved.");
         }
 
-        private static void Info(string[] args)
+        private static string GetParse()
         {
+            var result = new StringBuilder();
+
+            result.AppendLine($"Blocks: {_blocks.Length}");
+            for (var i = 0; i < _blocks.Length; i++)
+            {
+                result.AppendLine($"{i}: {_blocks[i]}");
+                for (var j = 0; j < _blocks[i].ResponseCount; j++)
+                {
+                    result.AppendLine($"  {j}: {_blocks[i].Responses[j]}");
+                }
+            }
+            return result.ToString();
+        }
+
+        private static string GetInfo()
+        {
+            var result = new StringBuilder();
+
             var responses = _blocks.SelectMany(x => x.Responses).ToArray();
             var sorted = responses.GroupBy(x => x.Type)
                 .Select(x => x.ToArray())
@@ -176,9 +140,9 @@
             for (var i = 0; i < sorted.Length; i++)
             {
                 var avarageSize = (int)Math.Floor(sorted[i].Average(x => x.ContentLength));
-                Console.WriteLine($"    {i}: {sorted[i][0].Type}");
-                Console.WriteLine($"      Count: {sorted[i].Length}");
-                Console.WriteLine($"      Average Size: {avarageSize}");
+                result.AppendLine($"{i}: {sorted[i][0].Type}");
+                result.AppendLine($"  Count: {sorted[i].Length}");
+                result.AppendLine($"  Average Size: {avarageSize}");
             }
 
             var firstDateTime = responses.FirstOrDefault(x => x.HasDateTime)?.DateTime;
@@ -186,12 +150,38 @@
             if (firstDateTime != null && lastDateTime != null)
             {
                 var sessionTime = lastDateTime - firstDateTime;
-                Console.WriteLine($"\n    Session time: {sessionTime?.ToString(@"hh\:mm\:ss")}\n");
+                result.AppendLine($"\nSession time: {sessionTime?.ToString(@"hh\:mm\:ss")}");
             }
             else
             {
-                Console.WriteLine("\n    Session time: Unknown\n");
+                result.AppendLine("\nSession time: Unknown");
             }
+            return result.ToString();
+        }
+
+        private static string GetResponse(int blockIndex, int responseIndex, Newtonsoft.Json.Formatting formatting)
+        {
+            var result = new StringBuilder();
+
+            var response = _blocks[blockIndex].Responses[responseIndex];
+            result.AppendLine($"  Type: {response.Type}");
+            if (response.HasDateTime)
+            {
+                result.AppendLine($"Time: {response.DateTime}");
+            }
+            result.AppendLine($"  Compression format: {response.CompressionFormat}");
+            result.AppendLine($"  Size (compressed): {response.Length}");
+            result.AppendLine($"  Size (decompressed): {response.ContentLength}");
+            if (formatting == Newtonsoft.Json.Formatting.None)
+            {
+                result.AppendLine($"  Content: {response.Content?.ToString(formatting)}");
+            }
+            else
+            {
+                result.AppendLine($"  Content:\n{response.Content?.ToString(formatting)}");
+            }
+            
+            return result.ToString();
         }
     }
 }
